34dfa64f44a1f9cc9039048988d49775c8e9ecdc
Davide Dolla <neoDD69@gmail.com>
2026-01-14 16:21:18 +0100

    . utils

--- _copyToDelfiRepo.cmd
+++ _copyToDelfiRepo.cmd
@@ -1,6 +1,6 @@
-xcopy ".\assets" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\assets" /E /I /Y /H
-xcopy ".\index.html" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\" /E /I /Y /H
-xcopy ".\robots.txt" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\" /E /I /Y /H
-xcopy ".\it" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\it" /E /I /Y /H
-xcopy ".\en" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\en" /E /I /Y /H
-xcopy ".\immagini" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\immagini" /E /I /Y /H
+xcopy ".\assets" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\site\assets" /E /I /Y /H
+xcopy ".\index.html" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\site" /E /I /Y /H
+xcopy ".\robots.txt" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\site" /E /I /Y /H
+xcopy ".\it" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\site\it" /E /I /Y /H
+xcopy ".\en" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\site\en" /E /I /Y /H
+xcopy ".\immagini" "C:\VsProjs\COMBINEpy\PRIVATE_SITE\PRIVATEcommunityWEBsite\PRIVATEcommunityWEBsite\site\immagini" /E /I /Y /H
